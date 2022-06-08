10499d9159ba53fa1e80bd9d5578b10dd1ad649d
niklasjohto <niklasjohto@tutanota.com>
2022-06-08 19:37:59 +0200

    Better motor reset for flipper

--- conrad.ts
+++ conrad.ts
@@ -12,34 +12,30 @@ forever(function () {
     motors.largeAD.run(100, 1000);
   } else if (atGameBorderLeft) {
     music.playSoundEffect(sounds.systemOverpower);
-    motors.largeAD.tank(-35, -55);
+    motors.largeAD.tank(-45, -75);
 
     sensors.color2.pauseUntilColorDetected(ColorSensorColor.Red);
     motors.largeAD.stop();
   } else if (atGameBorderRight) {
     music.playSoundEffect(sounds.systemOverpower);
-    motors.largeAD.tank(-55, -35);
+    motors.largeAD.tank(-75, -45);
 
     sensors.color2.pauseUntilColorDetected(ColorSensorColor.Red);
     motors.largeAD.stop();
   } else {
-    motors.largeAD.run(40);
+    motors.largeAD.run(70);
+  }
+
+  if (sensors.ultrasonic4.distance() <= 15) {
+    music.playSoundEffect(sounds.movementsSnap);
+    motors.largeAD.run(100);
+    motors.mediumB.run(100, 1250);
+    motors.mediumB.reset();
+    motors.mediumB.pauseUntilStalled();
   }
 
   brick.showNumber(sensors.ultrasonic4.distance(), 1);
   brick.showNumber(sensors.color1.color(), 2);
   brick.showNumber(sensors.color3.color(), 3);
   brick.showNumber(sensors.color2.color(), 4);
-});
-
-sensors.ultrasonic4.onEvent(UltrasonicSensorEvent.ObjectDetected, function () {
-  if (sensors.ultrasonic4.distance() <= 16) {
-    if (!atGameBorderLeft || !atGameBorderRight) {
-      motors.largeAD.run(100);
-    }
-
-    music.playSoundEffect(sounds.movementsSnap);
-    motors.mediumB.run(100, 1250);
-    motors.mediumB.run(-100, 125);
-  }
 });
