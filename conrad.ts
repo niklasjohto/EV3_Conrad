3498d1d0f9ee60f5516ecde8210e21ed6bc25894
niklasjohto <niklasjohto@tutanota.com>
2022-06-12 14:38:12 +0200

    Sensor & Motor adjustments

--- conrad.ts
+++ conrad.ts
@@ -1,6 +1,6 @@
 let atGameBorderLeft = sensors.color1.isColorDetected(5);
 let atGameBorderRight = sensors.color3.isColorDetected(5);
-let atGameBorderBehind = sensors.color2.isColorDetected(5);
+eBorderBehind = sensors.color2.isColorDetected(5);
 
 forever(function () {
   atGameBorderLeft = sensors.color1.isColorDetected(5);
@@ -9,33 +9,39 @@ forever(function () {
 
   if (atGameBorderBehind) {
     music.playSoundEffect(sounds.movementsSpeedUp);
-    motors.largeAD.run(100, 1000);
+
+    motors.largeAD.run(100);
   } else if (atGameBorderLeft) {
     music.playSoundEffect(sounds.systemOverpower);
-    motors.largeAD.tank(-45, -75);
+    motors.largeAD.tank(-40, -100);
 
     sensors.color2.pauseUntilColorDetected(ColorSensorColor.Red);
     motors.largeAD.stop();
   } else if (atGameBorderRight) {
     music.playSoundEffect(sounds.systemOverpower);
-    motors.largeAD.tank(-75, -45);
+    motors.largeAD.tank(-100, -40);
 
     sensors.color2.pauseUntilColorDetected(ColorSensorColor.Red);
     motors.largeAD.stop();
   } else {
-    motors.largeAD.run(70);
-  }
-
-  if (sensors.ultrasonic4.distance() <= 15) {
-    music.playSoundEffect(sounds.movementsSnap);
-    motors.largeAD.run(100);
-    motors.mediumB.run(100, 1250);
-    motors.mediumB.reset();
-    motors.mediumB.pauseUntilStalled();
+    motors.largeAD.run(80);
   }
 
   brick.showNumber(sensors.ultrasonic4.distance(), 1);
   brick.showNumber(sensors.color1.color(), 2);
   brick.showNumber(sensors.color3.color(), 3);
   brick.showNumber(sensors.color2.color(), 4);
+});
+
+sensors.ultrasonic4.onEvent(UltrasonicSensorEvent.ObjectDetected, function () {
+  if (
+    sensors.ultrasonic4.distance() >= 10 &&
+    sensors.ultrasonic4.distance() <= 15
+  ) {
+    music.playSoundEffect(sounds.movementsSnap);
+    motors.largeB.run(100, 500);
+
+    motors.largeB.pauseUntilStalled();
+    motors.largeB.reset();
+  }
 });
